2535641d3df192e2a13c07fef69458aeeebefcf9
Corentin Girard <corentingirard.dev@gmail.com>
2021-03-28 00:06:36 +0100

    Add entry point for package

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import {
+    convertData,
+    convertMatchStatus,
+    convertParticipant,
+    convertParticipantResult,
+    convertRoundRobinMode,
+    convertStageSettings,
+    convertStageType,
+} from "./convert";
+
+import { Database, toornament } from "./types";
+
+export {
+    convertData,
+    convertMatchStatus,
+    convertParticipant,
+    convertParticipantResult,
+    convertRoundRobinMode,
+    convertStageSettings,
+    convertStageType,
+    Database,
+    toornament,
+}

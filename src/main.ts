5f80f5f674e9f107433e20b4fafeffc8369d383b
Corentin Girard <corentingirard.dev@gmail.com>
2021-12-02 02:12:27 +0100

    Create input and output folders

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { convertData } from './convert';
 
-const data = JSON.parse(readFileSync('toornament.json', { encoding: 'utf-8' }));
+const data = JSON.parse(readFileSync('input/toornament.json', { encoding: 'utf-8' }));
 const result = convertData(data);
 
-writeFileSync('db.json', JSON.stringify(result.database, null, 4));
-writeFileSync('mappings.json', JSON.stringify(result.mappings, null, 4));
+writeFileSync('output/db.json', JSON.stringify(result.database, null, 4));
+writeFileSync('output/mappings.json', JSON.stringify(result.mappings, null, 4));
